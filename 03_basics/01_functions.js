97bc178976dfd2cee727c1edf5579f154a11d041
5053akg <5053akg@gmail.com>
2026-06-27 18:31:45 +0000

    continues

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -52,3 +52,48 @@ function loginUserMessage(username){
 // console.log(loginUserMessage("Anand"))
 // console.log(loginUserMessage(""))
 console.log(loginUserMessage())
+
+
+// shopping cart typ example
+
+function calculateCartPrice(...num1){
+    return num1
+}
+
+console.log(calculateCartPrice(200, 400, 600, 500));
+
+
+
+
+function calculateCartPrice2(val1, val2, ...num1){
+    return num1
+}
+
+console.log(calculateCartPrice2(200, 400, 600, 500));
+
+
+const user = {
+    username: "Anand",
+    price: 299
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+handleObject(user)
+handleObject({
+    username: "ABCDEF",
+    price: 999
+})
+
+
+const myNewArray = [200, 400, 600, 150]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
+console.log([200, 400, 100, 600]);
+
