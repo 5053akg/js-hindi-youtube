81071f2e5d13feac8331e48c530b0c2cb79a0976
akg5053 <akg5053@gmail.com>
2026-07-02 09:50:32 +0530

    continues

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,8 +1,57 @@
-let a = 10
-const b = 20
-var c = 30
 
-console.log(a)
-console.log(b);
-console.log(c);
+var c = 300
+let a = 300
 
+if(true){
+    let a = 10
+    const b = 20
+    var c = 30
+}
+
+// console.log(a)
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "Anand"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    //console.log(website);
+
+    two();
+}
+
+one(); 
+
+if(true){
+    const username = "Anand"
+    if(username=="Anand"){
+        const website = "youtube"
+        console.log(username + website);
+    }
+    console.log(website);
+    
+}
+
+console.log(username );
+
+
+// ++++++++++++++++++++++++ interesting ++++++++++
+
+
+console.log(addOne(8));
+function addone(num){
+    return num+1
+}
+addone(5)
+
+
+console.log(addTwo(8));
+const addTwo = function(num){
+    return num+2
+}
+addTwo(5)
